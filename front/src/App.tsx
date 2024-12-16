28c9c4477aa464b4d0df9e2b51db03b6932ba3a5
qqssddaaww <xkdlahxlr@naver.com>
2024-12-16 17:07:28 +0900

    front v4 react-router-dom v7

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router";
+import { createBrowserRouter, RouterProvider } from "react-router";
 import List from "./component/board/List";
 import View from "./component/board/View";
 import Write from "./component/board/Write";
@@ -11,28 +11,31 @@ import { checkSession } from "./component/store/userSlice";
 import { AppDispatch, RootState } from "./component/store/store";
 
 export default function App() {
-  
   const dispatch = useDispatch<AppDispatch>();
-  const {user} = useSelector((state:RootState) => state.user);
-  
+  const { user } = useSelector((state: RootState) => state.user);
+
   useEffect(() => {
     dispatch(checkSession());
-    console.dir(user)
-  },[dispatch])
+    console.dir(user);
+  }, [dispatch]);
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        { path: "/", element: <List /> },
+        { path: "write", element: <Write /> },
+        { path: "view/:uid", element: <View /> },
+        { path: "login", element: <Login /> },
+        { path: "/join", element: <Join />},
+      ]
+    }
+  ])
 
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<List />} />
-          <Route element={<Layout />}>
-            <Route path="/write" element={<Write />} />
-            <Route path="/view/:uid" element={<View />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/join" element={<Join/>}/>
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <RouterProvider router={router} />
     </>
   );
 }
