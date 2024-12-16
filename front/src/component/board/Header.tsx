2220e776f7d2ba75149824e0c33f129962362d21
qqssddaaww <xkdlahxlr@naver.com>
2024-12-16 17:14:45 +0900

    front v4 header merge

--- front/src/component/board/Header.tsx
+++ front/src/component/board/Header.tsx
@@ -1,13 +1,34 @@
 import { Link } from "react-router";
 
 import "./css/list.scss";
+import useAppSelector from "../hooks/useAppSelector";
+import { logoutUser } from "../store/userSlice";
+import useRoute from "../hooks/useRoute";
 
 export default function Header() {
+  const { user, dispatch } = useAppSelector();
+  const { navigate } = useRoute();
+
+  const handlelogout = () => {
+    dispatch(logoutUser());
+    navigate("/");
+  };
   return (
-    <div className="header-div">
+    <div className="list-header-div">
       <Link to={"/"} style={{ textDecoration: "none" }}>
         <h2>게시판</h2>
       </Link>
+      <div>
+        {user.id === "" ? (
+          <Link to={"/login"} style={{ textDecoration: "none" }}>
+            <button className="list-login-button">로그인</button>
+          </Link>
+        ) : (
+          <button className="list-login-button" onClick={handlelogout}>
+            {user.name}
+          </button>
+        )}
+      </div>
     </div>
   );
 }
