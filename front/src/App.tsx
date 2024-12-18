90700389f36e35bc798c0df9830bfbe7c8c31f0f
qqssddaaww <xkdlahxlr@naver.com>
2024-12-18 15:00:39 +0900

    react-query v1

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -5,11 +5,8 @@ import Write from "./component/board/Write";
 import Login from "./component/board/Login";
 import Layout from "./component/board/Layout";
 import Join from "./component/board/Join";
-import { SWRConfig } from "swr";
-import axiosInstance from './utils/axiosInstance';
 
 export default function App() {
-  const fetcher = (url: string) => axiosInstance.get(url).then((res) => res.data);
 
   const router = createBrowserRouter([
     {
@@ -27,9 +24,7 @@ export default function App() {
 
   return (
     <>
-      <SWRConfig value={{ fetcher, revalidateOnFocus: true, revalidateOnReconnect: true,  refreshInterval: 5000,  }}>
         <RouterProvider router={router} />
-      </SWRConfig>
     </>
   );
 }
